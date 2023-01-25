3506b386840bee8f74b3aab710fd9caf6b32268c
Mark Wainwright <wainwrightml@gmail.com>
2023-01-25 18:40:49 +0000

    made tilegrid implement flippable and rotatable

--- src/tile_grid.rs
+++ src/tile_grid.rs
@@ -10,7 +10,7 @@ use crate::prelude::*;
 use serde::{Deserialize, Serialize};
 
 macro_rules! tile_grid {
-    ($name:ident, $point_ty:ident, $inner:ty) => {
+    ($name:ident, $tile:ident, $inner:ty) => {
         #[must_use]
         #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
         #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -25,16 +25,16 @@ macro_rules! tile_grid {
             for $name<T, W, H, SIZE>
         {
             fn default() -> Self {
-                debug_assert!(SIZE == (W * H )as usize);
+                debug_assert!(SIZE == (W * H) as usize);
                 Self([T::default(); SIZE])
             }
         }
 
         impl<T, const W: $inner, const H: $inner, const SIZE: usize> $name<T, W, H, SIZE> {
             #[must_use]
-            pub fn from_fn<F : FnMut($point_ty::<W, H>) -> T>(mut cb: F)->Self{
-                debug_assert!(SIZE == (W * H )as usize);
-                let arr = core::array::from_fn(|i| cb($point_ty::try_from_usize(i).unwrap()) );
+            pub fn from_fn<F: FnMut($tile<W, H>) -> T>(mut cb: F) -> Self {
+                debug_assert!(SIZE == (W * H) as usize);
+                let arr = core::array::from_fn(|i| cb($tile::try_from_usize(i).unwrap()));
                 Self(arr)
             }
 
@@ -43,51 +43,25 @@ macro_rules! tile_grid {
             pub fn into_inner(self) -> [T; SIZE] {
                 self.0
             }
-            
+
             #[must_use]
             #[inline]
             pub fn from_inner(inner: [T; SIZE]) -> Self {
-                debug_assert!(SIZE == (W * H )as usize);
+                debug_assert!(SIZE == (W * H) as usize);
                 Self(inner)
             }
 
             #[must_use]
             #[inline]
-            pub fn enumerate(&self) -> impl iter::Iterator<Item = ($point_ty<W, H>, &'_ T)> {
+            pub fn enumerate(&self) -> impl iter::Iterator<Item = ($tile<W, H>, &'_ T)> {
                 self.0
                     .iter()
                     .enumerate()
-                    .map(|(inner, x)| ($point_ty::try_from_usize(inner).unwrap(), x))
-            }
-
-            /// Flip along the y-axis.
-            /// Th leftmost column becomes the rightmost column
-            pub fn flip_horizontal(&mut self) {
-                for y in 0..H {
-                    for x in 0..W / 2 {
-                        let p1 = $point_ty::<W, H>::new_unchecked(x,y);
-                        let mut p2 = p1;
-                        p2.flip_horizontal();
-                        self.swap(p1, p2);
-                    }
-                }
-            }
-
-            /// Flip along the x axis.
-            /// The top row becomes the bottom row
-            pub fn flip_vertical(&mut self) {
-                for y in 0..H / 2 {
-                    for x in 0..W {
-                        let p1 = $point_ty::<W, H>::try_new(x, y).unwrap();
-                        let mut p2 = p1;
-                        p2.flip_vertical();
-                        self.swap(p1, p2);
-                    }
-                }
+                    .map(|(inner, x)| ($tile::try_from_usize(inner).unwrap(), x))
             }
 
             #[inline]
-            pub fn swap(&mut self, p1: $point_ty<W, H>, p2: $point_ty<W, H>) {
+            pub fn swap(&mut self, p1: $tile<W, H>, p2: $tile<W, H>) {
                 self.0.swap(p1.into(), p2.into());
             }
 
@@ -136,63 +110,113 @@ macro_rules! tile_grid {
             }
         }
 
-        impl<T, const L: $inner, const SIZE: usize> $name<T, L, L, SIZE> {
-            pub fn rotate_clockwise(&mut self) {
-                for row in 0..=(L / 2) {
-                    for column in row..=(L / 2) {
-                        let o_row = L - (1 + row);
-                        let o_column = L - (1 + column);
-                        if row != o_row || column != o_column {
-                            let p0 = $point_ty::new_unchecked(column, row);
-                            let p1 = $point_ty::new_unchecked(row, o_column);
-                            let p2 = $point_ty::new_unchecked(o_column, o_row);
-                            let p3 = $point_ty::new_unchecked(o_row, column);
-
-                            //0123
-                            self.swap(p0, p3); //3120
-                            self.swap(p0, p1); //1320
-                            self.swap(p1, p2); //1230
-                        }
+        impl<T, const COLS: $inner, const ROWS: $inner, const SIZE: usize> Flippable
+            for $name<T, COLS, ROWS, SIZE>
+        {
+            fn flip_horizontal(&mut self) {
+                for y in 0..ROWS {
+                    for x in 0..COLS / 2 {
+                        let p1 = $tile::<COLS, ROWS>::new_unchecked(x, y);
+                        let mut p2 = p1;
+                        p2.flip_horizontal();
+                        self.swap(p1, p2);
                     }
                 }
             }
 
-            pub fn rotate_anticlockwise(&mut self) {
-                for row in 0..=(L / 2) {
-                    for column in row..=(L / 2) {
-                        let o_row = L - (1 + row);
-                        let o_column = L - (1 + column);
-                        if row != o_row || column != o_column {
-                            let p0 = $point_ty::new_unchecked(column, row);
-                            let p1 = $point_ty::new_unchecked(row, o_column);
-                            let p2 = $point_ty::new_unchecked(o_column, o_row);
-                            let p3 = $point_ty::new_unchecked(o_row, column);
+            fn flip_vertical(&mut self) {
+                for y in 0..ROWS / 2 {
+                    for x in 0..COLS {
+                        let p1 = $tile::<COLS, ROWS>::try_new(x, y).unwrap();
+                        let mut p2 = p1;
+                        p2.flip_vertical();
+                        self.swap(p1, p2);
+                    }
+                }
+            }
+        }
 
-                            //0123
-                            self.swap(p1, p2); //0213
-                            self.swap(p0, p1); //2013
-                            self.swap(p0, p3); //3012
+        impl<T, const L: $inner, const SIZE: usize> Rotatable for $name<T, L, L, SIZE> {
+            fn rotate(&mut self, quarter_turns: QuarterTurns) {
+                match quarter_turns {
+                    QuarterTurns::Zero => {
+                        return;
+                    }
+                    QuarterTurns::One => {
+                        for row in 0..=(L / 2) {
+                            for column in row..=(L / 2) {
+                                let o_row = L - (1 + row);
+                                let o_column = L - (1 + column);
+                                if row != o_row || column != o_column {
+                                    let p0 = $tile::new_unchecked(column, row);
+                                    let p1 = $tile::new_unchecked(row, o_column);
+                                    let p2 = $tile::new_unchecked(o_column, o_row);
+                                    let p3 = $tile::new_unchecked(o_row, column);
+
+                                    //0123
+                                    self.swap(p0, p3); //3120
+                                    self.swap(p0, p1); //1320
+                                    self.swap(p1, p2); //1230
+                                }
+                            }
+                        }
+                    }
+                    QuarterTurns::Two => {
+                        for row in 0..=(L / 2) {
+                            for column in row..=(L / 2) {
+                                let o_row = L - (1 + row);
+                                let o_column = L - (1 + column);
+                                if row != o_row || column != o_column {
+                                    let p0 = $tile::new_unchecked(column, row);
+                                    let p1 = $tile::new_unchecked(row, o_column);
+                                    let p2 = $tile::new_unchecked(o_column, o_row);
+                                    let p3 = $tile::new_unchecked(o_row, column);
+
+                                    //0123
+                                    self.swap(p0, p3); //3120
+                                    self.swap(p1, p2); //3210
+                                }
+                            }
+                        }
+                    }
+                    QuarterTurns::Three => {
+                        for row in 0..=(L / 2) {
+                            for column in row..=(L / 2) {
+                                let o_row = L - (1 + row);
+                                let o_column = L - (1 + column);
+                                if row != o_row || column != o_column {
+                                    let p0 = $tile::new_unchecked(column, row);
+                                    let p1 = $tile::new_unchecked(row, o_column);
+                                    let p2 = $tile::new_unchecked(o_column, o_row);
+                                    let p3 = $tile::new_unchecked(o_row, column);
+
+                                    //0123
+                                    self.swap(p1, p2); //0213
+                                    self.swap(p0, p1); //2013
+                                    self.swap(p0, p3); //3012
+                                }
+                            }
                         }
                     }
                 }
             }
         }
 
-        impl<T, const W: $inner, const H: $inner, const SIZE: usize> Index<$point_ty<W, H>>
+        impl<T, const W: $inner, const H: $inner, const SIZE: usize> Index<$tile<W, H>>
             for $name<T, W, H, SIZE>
         {
             type Output = T;
 
-            fn index(&self, index: $point_ty<W, H>) -> &Self::Output {
+            fn index(&self, index: $tile<W, H>) -> &Self::Output {
                 let u: usize = index.into();
                 &self.0[u]
             }
         }
 
-        impl<T, const W: $inner, const H: $inner, const SIZE: usize> IndexMut<$point_ty<W, H>>
+        impl<T, const W: $inner, const H: $inner, const SIZE: usize> IndexMut<$tile<W, H>>
             for $name<T, W, H, SIZE>
         {
-            fn index_mut(&mut self, index: $point_ty<W, H>) -> &mut Self::Output {
+            fn index_mut(&mut self, index: $tile<W, H>) -> &mut Self::Output {
                 let u: usize = index.into();
                 &mut self.0[u]
             }
@@ -272,28 +296,25 @@ macro_rules! tile_grid {
     };
 }
 
-
 tile_grid!(TileGrid16, Tile16, u16);
 tile_grid!(TileGrid8, Tile8, u8);
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::prelude::*;
     use itertools::Itertools;
-    #[cfg(feature="serde")]
-    use serde_test::{Token, assert_tokens};
-
+    #[cfg(feature = "serde")]
+    use serde_test::{assert_tokens, Token};
 
     #[test]
     #[should_panic(expected = "assertion failed")]
-    fn test_bad_grid(){
+    fn test_bad_grid() {
         let mut grid: TileGrid8<usize, 3, 3, 10> = TileGrid8::default();
     }
 
     #[test]
-    fn test_flip_vertical(){
+    fn test_flip_vertical() {
         let mut grid: TileGrid8<usize, 3, 3, 9> = TileGrid8::from_fn(|x| x.into());
 
         assert_eq!(grid.to_string(), "0|1|2\n3|4|5\n6|7|8");
@@ -302,7 +323,7 @@ mod tests {
     }
 
     #[test]
-    fn test_flip_horizontal(){
+    fn test_flip_horizontal() {
         let mut grid: TileGrid8<usize, 3, 3, 9> = TileGrid8::from_fn(|x| x.into());
 
         assert_eq!(grid.to_string(), "0|1|2\n3|4|5\n6|7|8");
@@ -315,13 +336,13 @@ mod tests {
         let mut grid: TileGrid8<usize, 3, 3, 9> = TileGrid8::from_fn(|x| x.into());
 
         assert_eq!(grid.to_string(), "0|1|2\n3|4|5\n6|7|8");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "6|3|0\n7|4|1\n8|5|2");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "8|7|6\n5|4|3\n2|1|0");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "2|5|8\n1|4|7\n0|3|6");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "0|1|2\n3|4|5\n6|7|8");
     }
 
@@ -330,13 +351,13 @@ mod tests {
         let mut grid: TileGrid8<usize, 4, 4, 16> = TileGrid8::from_fn(|x| x.into());
 
         assert_eq!(grid.to_string(), "0|1|2|3\n4|5|6|7\n8|9|10|11\n12|13|14|15");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "12|8|4|0\n13|6|10|1\n14|5|9|2\n15|11|7|3");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "15|14|13|12\n11|10|9|8\n7|6|5|4\n3|2|1|0");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "3|7|11|15\n2|9|5|14\n1|10|6|13\n0|4|8|12");
-        grid.rotate_clockwise();
+        grid.rotate(QuarterTurns::One);
         assert_eq!(grid.to_string(), "0|1|2|3\n4|5|6|7\n8|9|10|11\n12|13|14|15");
     }
 
@@ -345,19 +366,19 @@ mod tests {
         let mut grid: TileGrid8<usize, 3, 3, 9> = TileGrid8::from_fn(|x| x.into());
 
         assert_eq!(grid.to_string(), "0|1|2\n3|4|5\n6|7|8");
-        grid.rotate_anticlockwise();
+        grid.rotate(QuarterTurns::Three);
         assert_eq!(grid.to_string(), "2|5|8\n1|4|7\n0|3|6");
-        grid.rotate_anticlockwise();
+        grid.rotate(QuarterTurns::Three);
         assert_eq!(grid.to_string(), "8|7|6\n5|4|3\n2|1|0");
-        grid.rotate_anticlockwise();
+        grid.rotate(QuarterTurns::Three);
         assert_eq!(grid.to_string(), "6|3|0\n7|4|1\n8|5|2");
-        grid.rotate_anticlockwise();
+        grid.rotate(QuarterTurns::Three);
         assert_eq!(grid.to_string(), "0|1|2\n3|4|5\n6|7|8");
     }
 
     #[test]
     fn basic_tests() {
-        let mut grid: TileGrid8<usize, 3, 3, 9> = TileGrid8::from_fn(|x|x.into());
+        let mut grid: TileGrid8<usize, 3, 3, 9> = TileGrid8::from_fn(|x| x.into());
 
         for i in 0..9 {
             assert_eq!(grid[Tile8::<3, 3>::try_from_usize(i).unwrap()], i)
@@ -370,19 +391,22 @@ mod tests {
         assert_eq!(s_flat, "0|1|2|3|4|5|6|7|8");
     }
 
-    #[cfg(feature="serde")]
+    #[cfg(feature = "serde")]
     #[test]
-    fn test_serde(){
-        let grid: TileGrid8<usize, 2, 2, 4> = TileGrid8::from_fn(|x|x.into());
+    fn test_serde() {
+        let grid: TileGrid8<usize, 2, 2, 4> = TileGrid8::from_fn(|x| x.into());
 
-        assert_tokens(&grid, &[
-            Token::NewtypeStruct { name: "TileGrid8", },
-            Token::Tuple { len: 4 },
-            Token::U64(0),
-            Token::U64(1),
-            Token::U64(2),
-            Token::U64(3),
-            Token::TupleEnd
-        ]);
+        assert_tokens(
+            &grid,
+            &[
+                Token::NewtypeStruct { name: "TileGrid8" },
+                Token::Tuple { len: 4 },
+                Token::U64(0),
+                Token::U64(1),
+                Token::U64(2),
+                Token::U64(3),
+                Token::TupleEnd,
+            ],
+        );
     }
 }
